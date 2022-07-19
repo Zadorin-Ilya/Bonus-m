71914c66b7fe4b915f7c3fa3f2ef7b2dc7dabbec
Илья Задорин <zadorinilja@yandex.ru>
2022-07-19 16:18:51 +0400

    Zadanie..

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Start");
+
+        int x = 600; // Стоимость билета
+        int i = 20; // Стоимость за одну милю от купленого билета
+
+        int z = x / i; // Количество полученх миль от купленого билета
+
+        System.out.println(z + " Количество полученх миль от купленого билета ");
+
+
+        System.out.println("Finish");
+    }
+}
